1b17479c0403020b886c42aca76c27bcfa4014e0
Erik Stenberg <estenberg@spotify.com>
2019-04-02 21:37:56 +0200

    requireNamespace instd of library

--- R/methods.R
+++ R/methods.R
@@ -15,18 +15,22 @@ print.mSPRT <- function(x,...){
 ##################
 
 plot.mSPRT <- function(x){
-  library(ggplot2)
-  xp <- as.data.frame(x$spr)
-  colnames(xp) <- "spr"
-  ggplot(xp, aes(y = spr, x = 1:nrow(xp)))+
-    geom_line()+
-    ylab("Probability Ratio")+
-    xlab("Observations Collected")+
-    geom_hline(yintercept = x$alpha^(-1))+
-    ylim(c(0,max(x$alpha^(-1)+2,max(x$spr))))+
-    theme_minimal()+
-    labs(title="Mixture Sequential Probability Ratio Test",
-         subtitle = ifelse(x$n.rejection < nrow(xp),
-                           paste0("Null Hypothesis Rejected After ",x$n.rejection, " Observations"),
-                           paste0("Null Hypothesis Accepted")))
+  if (requireNamespace("ggplot2", quietly = TRUE)) {
+    xp <- as.data.frame(x$spr)
+    colnames(xp) <- "spr"
+    ggplot2::ggplot(xp, aes(y = spr, x = 1:nrow(xp)))+
+      geom_line()+
+      ylab("Probability Ratio")+
+      xlab("Observations Collected")+
+      geom_hline(yintercept = x$alpha^(-1))+
+      ylim(c(0,max(x$alpha^(-1)+2,max(x$spr))))+
+      theme_minimal()+
+      labs(title="Mixture Sequential Probability Ratio Test",
+           subtitle = ifelse(x$n.rejection < nrow(xp),
+                             paste0("Null Hypothesis Rejected After ",x$n.rejection, " Observations"),
+                             paste0("Null Hypothesis Accepted")))  } 
+  else {
+    plot(ts(x$spr))
+  }
+ 
 }
