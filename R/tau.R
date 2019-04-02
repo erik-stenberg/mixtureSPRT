677cc2a06979e4d0118691d2f4a34927ee504788
Erik Stenberg <estenberg@spotify.com>
2019-04-02 22:29:54 +0200

    ..

--- R/tau.R
+++ R/tau.R
@@ -5,6 +5,7 @@
 #' @param truncation Desired truncation time for mSPRT
 #' @return tau Optimal mixture variance \eqn{\tau} for mSPRT.
 #' @references \emph{Johari, R., Koomen, P., Pekelis, L. & Walsh, D. 2017, "Peeking at A/B Tests: Why it matters, and what to do about it", ACM, , pp. 1517} 
+#' @export
 calcTau <- function(alpha, sigma, truncation) {
   is.numeric(alpha) & alpha > 0 & alpha < 1 || stop("Alpha must be between 0 and 1")
   b <- (2*log(alpha^(-1)))/(truncation*sigma^2)^(1/2)
