5c9c7469a1ae2466825a7b2ed7642ef975e0d5c7
Erik Stenberg <estenberg@spotify.com>
2019-04-10 09:39:22 +0200

    print update

--- R/print.mSPRT.R
+++ R/print.mSPRT.R
@@ -4,6 +4,15 @@
 #' @param ... Further arguments
 #' @export
 print.mSPRT <- function(x,...){
-  cat("Decision: ", x$decision,"\n")
-  cat(x$text)
+
+  cat(" Decision: ", x$decision,"\n",
+      x$text)
+  cat(if(x$decision == "Accept H0"){" (truncated)"},
+      "\n", "Distribution:", x$distribution,"\n",
+      "Significance level:", x$alpha, "\n")
+  if(max(x$spr) < x$alpha^(-1)){
+    cat(" Probability Ratio after last observation:", round(max(x$spr),2),"\n",
+        "Rejection Region: ", "Prob. Ratio > ", x$alpha^(-1))
+  }
 }
+
