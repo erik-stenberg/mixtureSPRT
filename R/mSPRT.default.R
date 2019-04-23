22f93315db542b7db1b210e869e3ca4b70e2070c
Erik Stenberg <estenberg@spotify.com>
2019-04-23 11:18:27 +0200

    control-variates for normal distribution

--- R/mSPRT.default.R
+++ R/mSPRT.default.R
@@ -76,8 +76,8 @@ mSPRT.default <- function(x, y, xpre = NULL, ypre = NULL, sigma, tau, theta=0, d
       
       
       ### Bernoulli ###
-    
-      } else if (distribution == "bernoulli") {
+      
+    } else if (distribution == "bernoulli") {
       
       out <- mixtureSPRT::cppmSPRT(x = x,
                                    y = y,
@@ -99,77 +99,97 @@ mSPRT.default <- function(x, y, xpre = NULL, ypre = NULL, sigma, tau, theta=0, d
   
   else if(useCpp == F){
     
-    
     ### Normal ###
     
     out <- matrix(NA,length(x))
     if(distribution == "normal"){
-      
-      for(i in 1:length(z))
-        out[i] <- sqrt((2*sigma^2)/(2*sigma^2 + i *tau^2)) * exp(((i)^2*tau^2*(mean(z[1:i]) - theta)^2) / (4*sigma^2*(2*sigma^2 + i*tau^2)))  
-    }
-    
-    
-    ### Bernoulli ###
-    
-    if(distribution == "bernoulli"){
-      
-      if(!is.null(xpre) & !is.null(ypre)){
-
+     
+       if(!is.null(xpre) & !is.null(ypre)){
+        
         for(i in burnIn:length(x)){
+          
           k <- 0.5 * ((cov(xpre[1:i], x[1:i])/var(xpre[1:i])) + (cov(ypre[1:i], y[1:i])/var(ypre[1:i])))
           
           xn <- x[1:i] - k * xpre[1:i]
           yn <- y[1:i] - k * ypre[1:i]
           
-          Vn <- mean(xn[1:i]) * (1 - mean(xn[1:i]))  +   mean(yn[1:i]) * (1 - mean(yn[1:i]))
+          out[i] <- sqrt((2*sigma)/(2*sigma + i*tau^2)) * exp(((i)^2*tau^2*(mean(xn[1:i])-mean(yn[1:i]) - theta)^2) / (4*sigma*(2*sigma + i*tau^2)))  
           
-          out[i] <- sqrt((Vn)/(Vn + i*tau^2)) * exp(((i)^2*tau^2*(mean(xn[1:i])-mean(yn[1:i]) - theta)^2) / (2*Vn*(Vn + i*tau^2)))  
         }
-      } else if( is.null(xpre) | is.null(ypre)){
+         out[1:burnIn] <- 0
         
+      } else if( is.null(xpre) | is.null(ypre)) {
         
-          for(i in burnIn:length(z)){
-          Vn <- mean(x[0:i]) * (1-mean(x[0:i])) + mean(y[0:i]) * (1-mean(y[0:i]))
-          out[i] <- sqrt((Vn)/(Vn + i*tau^2)) * exp(((i)^2*tau^2*(mean(z[1:i]) - theta)^2) / (2*Vn*(Vn + i*tau^2)))  
+        for(i in 1:length(z)){
+          out[i] <- sqrt((2*sigma^2)/(2*sigma^2 + i*tau^2)) * exp(((i)^2*tau^2*(mean(z[1:i]) - theta)^2) / (4*sigma^2*(2*sigma^2 + i*tau^2)))  
         }
       }
-      out[1:burnIn] <- 1
+      
       out <- as.vector(out)
     }
+      
+      
+      ### Bernoulli ###
+      
+      else if(distribution == "bernoulli"){
+        
+        if(!is.null(xpre) & !is.null(ypre)){
+          
+          for(i in burnIn:length(x)){
+            k <- 0.5 * ((cov(xpre[1:i], x[1:i])/var(xpre[1:i])) + (cov(ypre[1:i], y[1:i])/var(ypre[1:i])))
+            
+            xn <- x[1:i] - k * xpre[1:i]
+            yn <- y[1:i] - k * ypre[1:i]
+            
+            Vn <- mean(xn[1:i]) * (1 - mean(xn[1:i]))  +   mean(yn[1:i]) * (1 - mean(yn[1:i]))
+            
+            out[i] <- sqrt((Vn)/(Vn + i*tau^2)) * exp(((i)^2*tau^2*(mean(xn[1:i])-mean(yn[1:i]) - theta)^2) / (2*Vn*(Vn + i*tau^2)))  
+          }
+        } else if( is.null(xpre) | is.null(ypre)){
+          
+          
+          for(i in burnIn:length(z)){
+            Vn <- mean(x[0:i]) * (1-mean(x[0:i])) + mean(y[0:i]) * (1-mean(y[0:i]))
+            out[i] <- sqrt((Vn)/(Vn + i*tau^2)) * exp(((i)^2*tau^2*(mean(z[1:i]) - theta)^2) / (2*Vn*(Vn + i*tau^2)))  
+          }
+        }
+        out[1:burnIn] <- 0
+        out <- as.vector(out)
+      }
+    }
+    
+    #################
+    
+    
+    ##########
+    # Output #
+    ##########
+    
+    
+    # Decision and text
+    n.rejection <- if(max(out,na.rm = T) > alpha^(-1)){
+      min(which(out>alpha^(-1)))
+    }else{
+      length(z)
+    }
+    
+    decision <- ifelse(n.rejection < length(x), paste0("Accept H1"), paste0("Accept H0"))
+    text <- paste0("Decision made after ",n.rejection," observations were collected")
+    
+    
+    output <- list(
+      distribution = distribution,
+      n = length(x),
+      spr = out,
+      n.rejection = n.rejection,
+      decision = decision,
+      text = text,
+      alpha = alpha
+    )
+    class(output) <- "mSPRT"
+    return(output)
+    
   }
   
-  #################
   
   
-  ##########
-  # Output #
-  ##########
-  
-  
-  # Decision and text
-  n.rejection <- if(max(out,na.rm = T) > alpha^(-1)){
-    min(which(out>alpha^(-1)))
-  }else{
-    length(z)
-  }
-  
-  decision <- ifelse(n.rejection < length(x), paste0("Accept H1"), paste0("Accept H0"))
-  text <- paste0("Decision made after ",n.rejection," observations were collected")
-  
-  
-  output <- list(
-    distribution = distribution,
-    n = length(x),
-    spr = out,
-    n.rejection = n.rejection,
-    decision = decision,
-    text = text,
-    alpha = alpha
-  )
-  class(output) <- "mSPRT"
-  return(output)
-  
-}
-
-
--- src/test.cpp
+++ src/test.cpp
@@ -12,87 +12,115 @@
 //' @export
 
 #include <Rcpp.h>
-#include<cmath>
-#include<string.h>
+#include <cmath>
+#include <string.h>
 using namespace Rcpp;
 using namespace std;
 
-
-double meanC(NumericVector x) {
+double meanC(NumericVector x)
+{
   int n = x.size();
   double total = 0;
-  
-  for(int i = 0; i < n; ++i) {
+
+  for (int i = 0; i < n; ++i)
+  {
     total += x[i];
   }
   return total / n;
 }
 
-
-double covC(NumericVector x, NumericVector y) { 
+double covC(NumericVector x, NumericVector y)
+{
   int n = x.size();
   double meany = mean(y);
   double meanx = mean(x);
-  NumericVector summa(n); 
-  for(int i = 0; i < n; i++) {
+  NumericVector summa(n);
+  for (int i = 0; i < n; i++)
+  {
     summa[i] = (x[i] - meanx) *
                (y[i] - meany);
   }
-  return sum(summa)/(n-1); 
+  return sum(summa) / (n - 1);
 }
 
-bool checkNull(Nullable<NumericVector> x) {
-  if (x.isNull()) {
+bool checkNull(Nullable<NumericVector> x)
+{
+  if (x.isNull())
+  {
     return true;
-  } else {
+  }
+  else
+  {
     return false;
   }
 }
 
-
-
 // [[Rcpp::export]]
-NumericVector cppmSPRT(Rcpp::NumericVector x, Rcpp::NumericVector y, Nullable<NumericVector> xpre, Nullable<NumericVector> ypre, double sigma, double tau, double theta=0, Rcpp::CharacterVector distribution = "normal"){
+NumericVector cppmSPRT(Rcpp::NumericVector x, Rcpp::NumericVector y, Nullable<NumericVector> xpre, Nullable<NumericVector> ypre, double sigma, double tau, double theta = 0, Rcpp::CharacterVector distribution = "normal")
+{
   int n = x.size();
   NumericVector out(n);
   double burnIn = 100;
   CharacterVector norm = "normal";
   CharacterVector bern = "bernoulli";
-  
-  
- if(checkNull(xpre) == false){
-   NumericVector xpre_n = NumericVector(xpre);
-   NumericVector ypre_n = NumericVector(ypre);
-    for(int i = burnIn; i < n; ++i){
-      double k = 0.5 * ((covC(xpre_n[Rcpp::Range(0,(i))], x[Rcpp::Range(0,(i))]) / var(xpre_n[Rcpp::Range(0,(i))])) +
-                  (covC(ypre_n[Rcpp::Range(0,(i))], y[Rcpp::Range(0,(i))])/var(ypre_n[Rcpp::Range(0,(i))])));
-      
-      NumericVector xn = x[Range(0,i)] - k * xpre_n[Range(0,i)];
-      NumericVector yn = y[Range(0,i)] - k * ypre_n[Range(0,i)];
-      
-      double Vn = mean(yn) * (1 - mean(yn)) + mean(xn) * (1-mean(xn));
-      //double newsigma = sqrt(Vn/2);
-      
-      out[i] = sqrt((Vn)/(Vn + (i+1)*pow(tau,2))) * exp((pow((i+1),2)*pow(tau,2)*pow((mean(xn)-mean(yn) - theta),2)) / (2*Vn*(Vn + (i+1)*pow(tau,2))));
-      }
-    return out;
-    
- } else {
-   
-   if(distribution[0] == norm[0]){
-     for(int i = (burnIn+1); i < n; ++i) {
-       out[i]  =  sqrt((2 * pow(sigma,2) / (2*pow(sigma,2) + (i+1) * pow(tau,2)))) * exp((pow((i+1),2)*pow(tau,2)*pow((mean(x[Rcpp::Range(0,(i+1))])- mean(y[Rcpp::Range(0,(i+1))]) - theta),2))/(4*pow(sigma,2)*(2*pow(sigma,2) + (i+1) * pow(tau,2))));
-     }
-   }
-   
-   else if(distribution[0] == bern[0]) {
-     for(int i = (burnIn+1); i < n; ++i) {
-       double Vn = mean(x[Range(0,i)]) * (1-mean(x[Range(0,i)])) + mean(y[Range(0,i)]) * (1-mean(y[Range(0,i)]));
-       out[i]    = sqrt((Vn)/(Vn + (i+1) * pow(tau,2))) * exp((pow((i+1),2)*pow(tau,2)*pow((mean(x[Rcpp::Range(0,(i))])- mean(y[Rcpp::Range(0,(i))]) - theta),2))/( 2 * Vn * ( Vn  + (i+1) * pow(tau,2))));
-     }
-   }
-  return out;
-  }
-  
-}
 
+  if (checkNull(xpre) == false)
+  {
+    NumericVector xpre_n = NumericVector(xpre);
+    NumericVector ypre_n = NumericVector(ypre);
+
+    if (distribution[0] == bern[0])
+    {
+      for (int i = burnIn; i < n; ++i)
+      {
+        double k = 0.5 * ((covC(xpre_n[Rcpp::Range(0, (i))], x[Rcpp::Range(0, (i))]) / var(xpre_n[Rcpp::Range(0, (i))])) +
+                          (covC(ypre_n[Rcpp::Range(0, (i))], y[Rcpp::Range(0, (i))]) / var(ypre_n[Rcpp::Range(0, (i))])));
+
+        NumericVector xn = x[Range(0, i)] - k * xpre_n[Range(0, i)];
+        NumericVector yn = y[Range(0, i)] - k * ypre_n[Range(0, i)];
+
+        double Vn = mean(yn) * (1 - mean(yn)) + mean(xn) * (1 - mean(xn));
+
+        out[i] = sqrt((Vn) / (Vn + (i + 1) * pow(tau, 2))) * exp((pow((i + 1), 2) * pow(tau, 2) * pow((mean(xn) - mean(yn) - theta), 2)) / (2 * Vn * (Vn + (i + 1) * pow(tau, 2))));
+      }
+    }
+
+    else if (distribution[0] == norm[0])
+    {
+      for (int i = burnIn; i < n; ++i)
+      {
+
+        double k = 0.5 * ((covC(xpre_n[Rcpp::Range(0, (i))], x[Rcpp::Range(0, (i))]) / var(xpre_n[Rcpp::Range(0, (i))])) +
+                          (covC(ypre_n[Rcpp::Range(0, (i))], y[Rcpp::Range(0, (i))]) / var(ypre_n[Rcpp::Range(0, (i))])));
+
+        NumericVector xn = x[Range(0, i)] - k * xpre_n[Range(0, i)];
+        NumericVector yn = y[Range(0, i)] - k * ypre_n[Range(0, i)];
+
+        out[i] = sqrt((2 * sigma) / (2 * sigma + (i + 1) * pow(tau, 2))) * exp((pow((i + 1), 2) * pow(tau, 2) * pow((mean(xn) - mean(yn) - theta), 2)) / (4 * sigma * (2 * sigma + (i + 1) * pow(tau, 2))));
+      }
+    }
+
+    return out;
+  }
+  else
+  {
+
+    if (distribution[0] == norm[0])
+    {
+      for (int i = 0; i < n; ++i)
+      {
+        out[i] = sqrt((2 * pow(sigma, 2) / (2 * pow(sigma, 2) + (i+1) * pow(tau, 2)))) * exp((pow((i+1 ), 2) * pow(tau, 2) * pow((mean(x[Rcpp::Range(0, (i))]) - mean(y[Rcpp::Range(0, (i))]) - theta), 2)) / (4 * pow(sigma, 2) * (2 * pow(sigma, 2) + (i+1 ) * pow(tau, 2))));
+      }
+    }
+
+    else if (distribution[0] == bern[0])
+    {
+      for (int i = 0; i < n; ++i)
+      {
+        double Vn = mean(x[Range(0, i)]) * (1 - mean(x[Range(0, i)])) + mean(y[Range(0, i)]) * (1 - mean(y[Range(0, i)]));
+        out[i] = sqrt((Vn) / (Vn + (i + 1) * pow(tau, 2))) * exp((pow((i + 1), 2) * pow(tau, 2) * pow((mean(x[Rcpp::Range(0, (i))]) - mean(y[Rcpp::Range(0, (i))]) - theta), 2)) / (2 * Vn * (Vn + (i + 1) * pow(tau, 2))));
+      }
+    }
+    return out;
+  }
+}
